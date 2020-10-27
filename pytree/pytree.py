163b1aec1a6c47f40148914d7c8fc31102142e34
Shen Yu <yu.shen@me.com>
2020-10-26 22:40:05 -0400

    Bug fixes in the delete, added more test cases, added documentation

--- pytree/pytree.py
+++ pytree/pytree.py
@@ -1,8 +1,12 @@
+from __future__ import annotations
+from typing import List
+
+
 class TreeNode(object):
     """
     Binary Search Tree Node
     """
-    def __init__(self, value):
+    def __init__(self, value: object):
         """
         :param value: value of the tree node
         """
@@ -10,6 +14,19 @@ class TreeNode(object):
         self.left = None
         self.right = None
         self.height = 1
+
+    def in_order_traversal(self) -> List[object]:
+        """
+        Implements in order binary tree traversal as defined in: https://en.wikipedia.org/wiki/Tree_traversal#In-order
+        :return: array containing
+        """
+        res = []
+        if self.left:
+            res += self.left.in_order_traversal()
+        res.append(self.value)
+        if self.right:
+            res += self.right.in_order_traversal()
+        return res
 
     def __str__(self):
         """
@@ -22,188 +39,231 @@ class TreeNode(object):
 
 class AVLTree(object):
     """
-    Python implementation of a self-balancing binary search tree.
+    Python implementation of a self-balancing binary search tree (AVL tree).
     """
     def __init__(self):
         self.root = None
 
-    @staticmethod
-    def update_height(node):
-        if node is not None:
-            node.height = 1 + max(AVLTree.get_height(node.left), AVLTree.get_height(node.right))
+    def insert_array(self, array: List[object]) -> AVLTree:
+        """
+        Insert an array of values into the AVLTree
+        :param array: array of values we want to insert into the AVLTree
+        :return: the updated AVLTree
+        """
+        for value in array:
+            self.insert(value)
+        return self
+
+    def insert(self, value: object) -> AVLTree:
+        """
+        Insert a single value into the AVLTree
+        :param value: input value we want to insert into the AVLTree
+        :return: the updated AVLTree
+        """
+        self.root = self._insert(self.root, value)
+        return self
+
+    def delete_array(self, array: List[object]) -> AVLTree:
+        """
+        Delete an array of values from the AVLTree
+        :param array: array of values we want to delete from the AVLTree
+        :return: the updated AVLTree
+        """
+        for value in array:
+            self.delete(value)
+        return self
+
+    def delete(self, value: object) -> AVLTree:
+        """
+        Delete a single value into the AVLTree
+        :param value: input value we want to delete from the AVLTree
+        :return: the updated AVLTree
+        """
+        self.root = self._delete(self.root, value)
+        return self
+
+    def _insert(self, node: TreeNode, value: object) -> TreeNode:
+        """
+        Detailed implementation of node insertion followed by self balancing when necessary
+        Average complexity O(log N), worst case O(log N)
+        :param node: root of the subtree on which to insert the input value
+        :param value: input value to insert into the subtree
+        :return: root of the subtree
+        """
+        # step 0: follow the same process as inserting into a Binary Search Tree
+        if node is None:
+            return TreeNode(value)
+        elif value < node.value:
+            node.left = self._insert(node.left, value)
+        else:
+            node.right = self._insert(node.right, value)
+
+        # step 1: update the height
+        self._update_height(node)
+
+        # step 2: compute the balance factor
+        balance = self._get_balance(node)
+
+        if balance < -1:
+            if value < node.left.value:
+                # step 3a: left left case
+                return self._rotate_right(node)
+            elif value > node.left.value:
+                # step 3b: left right case
+                node.left = self._rotate_left(node.left)
+                return self._rotate_right(node)
+        elif balance > 1:
+            if value > node.right.value:
+                # step 3c: right right case
+                return self._rotate_left(node)
+            elif value < node.right.value:
+                # step 3d: right left case
+                node.right = self._rotate_right(node.right)
+                return self._rotate_left(node)
+        return node
+
+    def _delete(self, node: TreeNode, value: object) -> TreeNode:
+        """
+        Detailed implementation of node deletion followed by self balancing when necessary
+        Average complexity O(log N), worst case O(log N)
+        :param node: root of the subtree on which to delete the input value
+        :param value: input value to delete from the subtree
+        :return: root of the subtree
+        """
+        # step 0: follow the same process as deleting from a Binary Search Tree
+        if node is None:
+            return node
+        elif value < node.value:
+            node.left = self._delete(node.left, value)
+        elif value > node.value:
+            node.right = self._delete(node.right, value)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            min_value = self._get_smallest_node(node.right).value
+            node.value = min_value
+            node.right = self._delete(node.right, min_value)
+
+        if node is None:
+            return node
+
+        # step 1: update the height
+        self._update_height(node)
+
+        # step 2: compute the balance factor
+        balance = self._get_balance(node)
+
+        if balance < -1:
+            if self._get_balance(node.right) >= 0:
+                # step 3a: left left case
+                return self._rotate_right(node)
+            else:
+                # step 3b: right left case
+                node.right = self._rotate_right(node.right)
+                return self._rotate_left(node)
+        elif balance > 1:
+            if self._get_balance(node.left) <= 0:
+                # step 3c: right right case
+                return self._rotate_left(node)
+            else:
+                # step 3d: left right case
+                node.left = self._rotate_left(node.left)
+                return self._rotate_right(node)
+        return node
+
+    def _rotate_right(self, node: TreeNode) -> TreeNode:
+        """
+        Before the right rotation:
+             node
+             /  \
+          root  T3
+          /  \
+        T1  T2
+
+        After the right rotation:
+          root
+          /  \
+        T1   node
+             /  \
+           T2   T3
+        :param node: node from which we want to perform the right rotation
+        :return: the right rotated subtree
+        """
+        root = node.left
+        node.left = root.right
+        root.right = node
+        self._update_height(node)
+        self._update_height(root)
+        return root
+
+    def _rotate_left(self, node: TreeNode) -> TreeNode:
+        """
+        Before the left rotation:
+          node
+          /  \
+        T1   root
+             /  \
+           T2   T3
+
+        After the left rotation:
+             root
+             /  \
+          node  T3
+          /  \
+        T1   T2
+        :param node: node from which we want to perform the left rotation
+        :return: the left rotated subtree
+        """
+        root = node.right
+        node.right = root.left
+        root.left = node
+        self._update_height(node)
+        self._update_height(root)
+        return root
 
     @staticmethod
-    def get_height(node):
+    def _get_height(node: TreeNode) -> int:
+        """
+        :param node: input node
+        :return: the height of the input node
+        """
         if node is None:
             return 0
         return node.height
 
-    @staticmethod
-    def get_balance(node):
+    def _update_height(self, node: TreeNode) -> None:
+        """
+        Update the height value for the input node assuming height of child node have already been updated
+        :param node: the input node on which we want to update teh height
+        :return: the updated height of the input node
+        """
+        if node is not None:
+            node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
+
+    def _get_balance(self, node: TreeNode) -> int:
         """
         :param node: input node
         :return: balance factor of the input node as defined by: https://en.wikipedia.org/wiki/AVL_tree#Balance_factor
         """
         if node is None:
             return 0
-        return AVLTree.get_height(node.right) - AVLTree.get_height(node.left)
+        return self._get_height(node.right) - self._get_height(node.left)
 
-    @staticmethod
-    def get_smallest_node(node):
+    def _get_smallest_node(self, node: TreeNode) -> TreeNode or None:
+        """
+        Traverse down from input node and return the smallest node in the subtree
+        :param node: the root of the subtree in which we want to find the smallest node
+        :return: the smallest node in the subtree
+        """
         if node is None or node.left is None:
             return node
-        return AVLTree.get_smallest_node(node.left)
-
-    @staticmethod
-    def rotate_right(node):
-        """
-             node
-             /  \
-          root  T3
-         /  \
-        T1  T2
-          root
-          /  \
-        T1   node
-             /  \
-            T2  T3
-        """
-        root = node.left
-        node.left = root.right
-        root.right = node
-        AVLTree.update_height(node)
-        AVLTree.update_height(root)
-        return root
-
-    @staticmethod
-    def rotate_left(node):
-        """
-          node
-          /  \
-        T1   root
-             /  \
-            T2  T3
-             root
-             /  \
-          node  T3
-         /  \
-        T1  T2
-        """
-        root = node.right
-        node.right = root.left
-        root.left = node
-        AVLTree.update_height(node)
-        AVLTree.update_height(root)
-        return root
-
-    @staticmethod
-    def insert(node, value):
-        # step 0: follow the same process as inserting into a Binary Search Tree
-        if node is None:
-            return TreeNode(value)
-        elif value < node.value:
-            node.left = AVLTree.insert(node.left, value)
-        else:
-            node.right = AVLTree.insert(node.right, value)
-
-        # step 1: update the height
-        AVLTree.update_height(node)
-
-        # step 2: compute the balance factor
-        balance = AVLTree.get_balance(node)
-
-        if balance < -1:
-            if value < node.left.value:
-                return AVLTree.rotate_right(node)
-            elif value > node.left.value:
-                node.left = AVLTree.rotate_left(node.left)
-                return AVLTree.rotate_right(node)
-        elif balance > 1:
-            if value > node.right.value:
-                return AVLTree.rotate_left(node)
-            elif value < node.right.value:
-                node.right = AVLTree.rotate_right(node.right)
-                return AVLTree.rotate_left(node)
-        return node
-
-    @staticmethod
-    def delete(node, value):
-        # step 0: follow the same process as deleting from a Binary Search Tree
-        if node is None:
-            return node
-        elif value < node.value:
-            node.left = AVLTree.delete(node.left, value)
-        elif value > node.value:
-            node.right = AVLTree.delete(node.right, value)
-        else:
-            if node.left is None:
-                return node.right
-            elif node.right is None:
-                return node.left
-            min_value = AVLTree.get_smallest_node(node.right)
-            node.value = min_value
-            node.right = AVLTree.delete(node.right, min_value)
-
-        if node is None:
-            return node
-
-        # step 1: update the height
-        AVLTree.update_height(node)
-
-        # step 2: compute the balance factor
-        balance = AVLTree.get_balance(node)
-
-        if balance < -1:
-            if AVLTree.get_balance(node.left) >= 0:
-                return AVLTree.rotate_right(node)
-            else:
-                node.left = AVLTree.rotate_left(node.left)
-                return AVLTree.rotate_right(node)
-        elif balance > 1:
-            if AVLTree.get_balance(node.right) < 0:
-                return AVLTree.rotate_left(node)
-            else:
-                node.right = AVLTree.rotate_right(node.right)
-                return AVLTree.rotate_left(node)
-        return node
+        return self._get_smallest_node(node.left)
 
 
 if __name__ == '__main__':
-    # left right
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 3)
-    print(tree.root)
-
-    # right left
-    tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 3)
-    tree.root = AVLTree.insert(tree.root, 2)
-    print(tree.root)
-
-    # left left
-    tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 3)
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 1)
-    print(tree.root)
-
-    # right right
-    tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 3)
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 5)
-    tree.root = AVLTree.insert(tree.root, 6)
-    tree.root = AVLTree.insert(tree.root, 7)
-    tree.root = AVLTree.delete(tree.root, 1)
-    tree.root = AVLTree.delete(tree.root, 2)
-    tree.root = AVLTree.delete(tree.root, 3)
+    tree.insert_array(["abc", "ab", "a"])
     print(tree.root)
 
--- tests/test_pytree.py
+++ tests/test_pytree.py
@@ -2,84 +2,237 @@ from pytree.pytree import AVLTree
 
 
 def test_update_height():
+    """
+    Test to make sure the height is being updated correctly:
+    Height = 3:       4
+                    /   \
+    Height = 2:   2       5
+                 / \\    / \
+    Height = 1: 1   3   6   7
+    """
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 4)
-    assert tree.root.height == 1
-    tree.root = AVLTree.insert(tree.root, 3)
-    assert tree.root.height == 2
-    tree.root = AVLTree.insert(tree.root, 2)
-    assert tree.root.height == 2
-    tree.root = AVLTree.insert(tree.root, 1)
+    tree.insert_array([1, 2, 3, 4, 5, 6, 7])
     assert tree.root.height == 3
-
-
-def test_get_height():
-    tree = AVLTree()
-    for i in range(7):
-        tree.root = AVLTree.insert(tree.root, i + 1)
-    assert AVLTree.get_height(tree.root) == 3
-    assert AVLTree.get_height(tree.root.left) == 2
-    assert AVLTree.get_height(tree.root.right) == 2
-    assert AVLTree.get_height(tree.root.left.left) == 1
-    assert AVLTree.get_height(tree.root.left.right) == 1
-    assert AVLTree.get_height(tree.root.right.left) == 1
-    assert AVLTree.get_height(tree.root.right.right) == 1
+    assert tree.root.left.height == 2
+    assert tree.root.right.height == 2
+    assert tree.root.left.left.height == 1
+    assert tree.root.left.right.height == 1
+    assert tree.root.right.left.height == 1
+    assert tree.root.right.right.height == 1
 
 
 def test_get_balance():
+    """
+    Test to make sure the balance is being calculated correctly:
+    Tree 1, balance on root is 0:
+      2
+    Tree 2, balance on root is -1:
+      2
+     /
+    1
+    Tree 3, balance on root is 0:
+      2
+     / \
+    1   3
+    Tree 4, balance on root is 1:
+      2
+     / \
+    1   3
+         \
+          4
+    """
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 2)
-    assert AVLTree.get_balance(tree.root) == 1
-    tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 1)
-    assert AVLTree.get_balance(tree.root) == -1
+    tree.insert(2)
+    assert tree._get_balance(tree.root) == 0
+    tree.insert(1)
+    assert tree._get_balance(tree.root) == -1
+    tree.insert(3)
+    assert tree._get_balance(tree.root) == 0
+    tree.insert(4)
+    assert tree._get_balance(tree.root) == 1
 
 
 def test_insert():
+    """
+    Test inserting the following tree:
+          4
+        /   \
+      2       5
+     / \\    / \
+    1   3   6   7
+    """
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 3)
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 5)
-    tree.root = AVLTree.insert(tree.root, 6)
-    tree.root = AVLTree.insert(tree.root, 7)
+    tree.insert(1).insert(2).insert(3).insert(4).insert(5).insert(6).insert(7)
+    assert str(tree.root) == "[[1, 2, 3], 4, [5, 6, 7]]"
+
+
+def test_delete():
+    """
+    Test deleting [4, ] from the following tree:
+          4
+        /   \
+      2       5
+     / \\    / \
+    1   3   6   7
+                 \
+                  8
+    Test deleting [1, 3] from the following tree:
+          6
+        /   \
+      4       7
+     / \\      \
+    2   5       8
+    Test deleting [7, 8] from the following tree:
+       4
+     /  \
+    2    6
+        /
+       5
+    Test deleting 4 from the following tree:
+       5
+     /  \
+    2    6
+    """
+    tree = AVLTree()
+    tree.insert_array([1, 2, 3, 4, 5, 6, 7, 8])
+    assert str(tree.root) == "[[1, 2, 3], 4, [5, 6, [, 7, 8]]]"
+    tree.delete_array([1, 3])
+    assert str(tree.root) == "[[2, 4, 5], 6, [, 7, 8]]"
+    tree.delete_array([7, 8])
+    assert str(tree.root) == "[2, 4, [5, 6, ]]"
+    tree.delete(4)
+    assert str(tree.root) == "[2, 5, 6]"
+
+
+def test_insert_array():
+    """
+    Test inserting the following tree using input array:
+          4
+        /   \
+      2       5
+     / \\    / \
+    1   3   6   7
+    """
+    tree = AVLTree()
+    tree.insert_array([1, 2, 3, 4, 5, 6, 7])
     assert str(tree.root) == "[[1, 2, 3], 4, [5, 6, 7]]"
 
 
 def test_left_right():
+    """
+    Test Tree for left right rotation:
+        5
+       / \
+      2   6
+     / \
+    1   3
+         \
+          4
+    After rotation, height = 3:
+        3
+       / \
+      2   5
+     /   / \
+    1   4   6
+    """
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 3)
-    assert str(tree.root) == "[1, 2, [3, 4, ]]"
+    tree.insert_array([5, 2, 6, 1, 3, 4])
+    assert tree.root.height == 3
+    assert str(tree.root) == "[[1, 2, ], 3, [4, 5, 6]]"
 
 
 def test_right_left():
+    """
+    Test Tree for right left rotation:
+      2
+     / \
+    1   5
+       / \
+      4   6
+     /
+    3
+    After rotation, height = 3:
+        4
+       / \
+      2   5
+     / \\  \
+    1   3   6
+    """
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 3)
-    tree.root = AVLTree.insert(tree.root, 2)
-    assert str(tree.root) == "[[, 1, 2], 3, 4]"
+    tree.insert_array([2, 1, 5, 4, 6, 3])
+    assert tree.root.height == 3
+    assert str(tree.root) == "[[1, 2, 3], 4, [, 5, 6]]"
 
 
 def test_right_right():
+    """
+    Test Tree for right right rotation:
+      2
+     / \
+    1   4
+       / \
+      3   5
+           \
+            6
+    After rotation, height = 2:
+        4
+       / \
+      2   5
+     / \\  \
+    1   3   6
+    """
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 1)
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 3)
-    tree.root = AVLTree.insert(tree.root, 4)
-    assert str(tree.root) == "[1, 2, [, 3, 4]]"
+    tree.insert_array([2, 1, 4, 3, 5, 6])
+    assert tree.root.height == 3
+    assert str(tree.root) == "[[1, 2, 3], 4, [, 5, 6]]"
 
 
 def test_left_left():
+    """
+    Test Tree for left left rotation:
+          5
+         / \
+        3   6
+       / \
+      2   4
+     /
+    1
+    After rotation, height = 3:
+        3
+       / \
+      2   5
+     /   / \
+    1   4   6
+    """
     tree = AVLTree()
-    tree.root = AVLTree.insert(tree.root, 4)
-    tree.root = AVLTree.insert(tree.root, 3)
-    tree.root = AVLTree.insert(tree.root, 2)
-    tree.root = AVLTree.insert(tree.root, 1)
-    assert str(tree.root) == "[[1, 2, ], 3, 4]"
+    tree.insert_array([5, 3, 6, 2, 4, 1])
+    assert tree.root.height == 3
+    assert str(tree.root) == "[[1, 2, ], 3, [4, 5, 6]]"
+
+
+def test_rebalance():
+    """
+    Testing deleting from a single side of the tree to see if the resulting tree remain balanced
+    """
+    tree = AVLTree()
+    tree.insert_array([i for i in range(10000)])
+    assert tree.root.height == 14
+    tree.delete_array([i for i in range(9900)])
+    assert tree.root.height == 7
+    tree = AVLTree()
+    tree.insert_array([i for i in range(10000)])
+    assert tree.root.height == 14
+    tree.delete_array([i for i in range(100, 10000)])
+    assert tree.root.height == 7
+
+
+def test_different_data_type():
+    """
+    Testing the AVL tree works on different data type
+    """
+    tree = AVLTree()
+    tree.insert_array(['abcd', 'xyz', 'cbz', 'abc', 'ab', 'a', 'abb'])
+    assert str(tree.root) == "[[a, ab, abb], abc, [abcd, cbz, xyz]]"
+    tree = AVLTree()
+    tree.insert_array([(3, 1), (1, 3), (2, 2), (2, 1), (1, 2), (2, 3), (3, 2)])
+    assert str(tree.root) == "[[(1, 2), (1, 3), (2, 1)], (2, 2), [(2, 3), (3, 1), (3, 2)]]"
